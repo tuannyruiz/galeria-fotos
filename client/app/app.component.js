3fefabdb12be3d6e4ad6c3366972cf61a4a107b0
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-03-25 12:05:28 -0300

    adiciona fotos vindas de uma api

--- client/app/app.component.js
+++ client/app/app.component.js
@@ -9,8 +9,18 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var http_1 = require('@angular/http');
 var AppComponent = (function () {
-    function AppComponent() {
+    function AppComponent(http) {
+        var _this = this;
+        this.photos = [];
+        http
+            .get('v1/fotos')
+            .map(function (res) { return res.json(); })
+            .subscribe(function (photos) {
+            _this.photos = photos;
+            console.log(_this.photos = photos);
+        }, function (erro) { return console.log(erro); });
     }
     AppComponent = __decorate([
         core_1.Component({
@@ -18,7 +28,7 @@ var AppComponent = (function () {
             selector: 'app',
             templateUrl: './app.component.html'
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [http_1.Http])
     ], AppComponent);
     return AppComponent;
 }());
--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Http} from '@angular/http';
 
 @Component({
   moduleId: module.id,
@@ -7,5 +8,14 @@ import {Component} from '@angular/core';
 })
 
 export class AppComponent {
-  
+  photos: Object[] = [];
+  constructor(http: Http) {
+    http
+    .get('v1/fotos')
+    .map(res => res.json())
+    .subscribe(photos => {
+      this.photos = photos;
+      console.log(this.photos = photos);  
+    }, erro => console.log(erro));
+  }
 }
